88bd07a8f15b659e761670b78f8ad4fdb660a8ad
Oryon <oryon@darou.fr>
2012-12-11 18:37:54 +0100

    Adding header in client

--- client_lib.h
+++ client_lib.h
@@ -5,8 +5,10 @@
 #ifndef __XEN_SHM_CLIENT_LIB_H__
 #define __XEN_SHM_CLIENT_LIB_H__
 
-#include "xen_shm_pipe.h"
 #include <netinet/in.h>
+
+#include "xen_shm_pipe.h"
+#include "handler_lib.h"
 
 int init_pipe(in_port_t distant_port, struct in_addr *distant_addr, xen_shm_pipe_p *receive_fd, xen_shm_pipe_p *send_fd, uint8_t proposed_page_page_count);
 
