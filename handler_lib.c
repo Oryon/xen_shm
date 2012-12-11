4f7d579fc23f4227ea81181e52f6edc9b34e0abb
Oryon <oryon@darou.fr>
2012-12-11 21:24:23 +0100

    Adding receiver function

--- handler_lib.c
+++ handler_lib.c
@@ -111,3 +111,44 @@ void* xen_shm_handler_sender(struct xen_shm_handler_data* data) {
 
 }
 
+
+void* xen_shm_handler_receiver(struct xen_shm_handler_data* data) {
+    struct xen_shm_handler_transfert* trans_data;
+    ssize_t ret;
+    struct timeval in_stamp;
+    struct timeval out_stamp;
+    uint64_t byte_counter;
+    uint32_t transfert_id;
+    uint64_t delay;
+    double bandwidth;
+
+
+    trans_data = (struct xen_shm_handler_transfert*) data->private_data;
+    byte_counter = 0;
+    transfert_id = xen_shm_handler_get_id();
+
+    gettimeofday(&in_stamp , NULL);
+    while(!data->stop) {
+        ret = xen_shm_pipe_read_all(data->send_fd, trans_data->buffer, trans_data->buffer_len);
+        if(ret < 0) {
+            printf("transfert %"PRIu32" error - ", transfert_id);
+            perror("pipe write all");
+            return NULL;
+        }
+        byte_counter+=trans_data->buffer_len;
+        if(trans_data->print_info && byte_counter>trans_data->print_interval_bytes) {
+            gettimeofday(&out_stamp , NULL);
+            delay = ((uint64_t) (out_stamp.tv_sec - in_stamp.tv_sec))*1000000 + (uint64_t) (out_stamp.tv_usec - in_stamp.tv_usec);
+            bandwidth = (((double) byte_counter)*8)/((double) delay);
+
+            printf("%"PRIu32", %"PRIu64", %f, Mbps",transfert_id, (uint64_t) out_stamp.tv_sec, bandwidth);
+            byte_counter = 0;
+            in_stamp = out_stamp;
+        }
+    }
+
+    return NULL;
+
+}
+
+
